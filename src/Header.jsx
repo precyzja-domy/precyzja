f562c885de8ce8768a05561f75fefa0099f5e959
precyzja-domy <klaragrochot1@gmail.com>
2025-10-11 18:53:52 +0200

    UI quickfix

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
   const closeMenu = () => setMenuOpen(false);
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-gray-950 text-gray-200 shadow-md z-50">
+    <header className="fixed top-0 left-0 w-full bg-gray-900 text-gray-200 shadow-md z-50">
       <div className="flex justify-between items-center px-6 py-4">
         {/* Logo */}
         <div className="flex items-center space-x-2">
